884b08a34419d59ffa6f5a54835a6f100d6132bf
Piotr Wojniak <61149682+PiotrWojniak@users.noreply.github.com>
2021-02-05 12:52:51 +0000

    add popup window after form was send

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,14 +1,22 @@
-function sendMail(contactForm){
+// Function adapted from CI tutorial for emailjs in mini-project walkthrough
+function sendMail(contactForm) {
     emailjs.send("piotr", "LearnTest", {
         "from_name": contactForm.name.value,
-        "from_email": contactForm.emailaddress.value,
-        "inforequest": contactForm.inforequest.value,
+        "from_emailaddress": contactForm.emailaddress.value,
+        "request_inforequest": contactForm.inforequest.value,
+       
     })
-    .then(
-        function(response) {
-            console.log("Success", response);
-        },
-    function(error){
-        console.log("Failed", error);
-    });
+        .then(
+            // display popup window after send message
+                function (response) {
+                    alert("Thank you", response);
+                    
+                },
+            // On error, using standard browser alert
+            function (error) {
+                alert("Sorry, it seems we have a problem. Please fill out the form and Submit again", error);
+            });
+            // Clear form after submission
+            document.getElementById('contactForm').reset();
+    return false;
 }
